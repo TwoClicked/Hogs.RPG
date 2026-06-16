ad8125d0764eec9c1851e359f022363e0e10201b
diego <Diego/HomeDesktop>
2026-06-16 10:53:50 +0200

    Update pet passives descriptions and remove DoubleStrike
    Revised descriptions for Lifesteal, GuardianShield, Thorns,
    and Executioner passives to clarify and enhance their effects.
    Increased Lifesteal healing to 10% of damage dealt. Removed
    the DoubleStrike passive entirely.

--- Hogs.RPG.Core/GameData/Pets/PetPassives.cs
+++ Hogs.RPG.Core/GameData/Pets/PetPassives.cs
@@ -1,6 +1,5 @@
 ﻿using Hogs.RPG.Core.Entities.PetObjects;
 using Hogs.RPG.Core.Enums;
-
 namespace Hogs.RPG.Core.GameData.Pets
 {
     public static class PetPassives
@@ -9,35 +8,31 @@ namespace Hogs.RPG.Core.GameData.Pets
         {
             Id = PetPassive.Lifesteal,
             Name = "Lifesteal",
-            Description = "Heal 5% of damage dealt"
+            Description = "Heal 10% of damage dealt"
         };
-
         public static readonly PetPassiveDefinition DoubleStrike = new()
         {
             Id = PetPassive.DoubleStrike,
             Name = "DoubleStrike",
             Description = "20% chance to deal double damage"
         };
-
         public static readonly PetPassiveDefinition GuardianShield = new()
         {
             Id = PetPassive.GuardianShield,
             Name = "GuardianShield",
-            Description = "Grants +10% defense"
+            Description = "15% chance to reduce incoming damage by 30%"
         };
-
         public static readonly PetPassiveDefinition Thorns = new()
         {
             Id = PetPassive.Thorns,
             Name = "Thorns",
-            Description = "Reflect 15% damage"
+            Description = "Reflect 15% of damage taken + 5 flat"
         };
-
         public static readonly PetPassiveDefinition Executioner = new()
         {
             Id = PetPassive.Executioner,
             Name = "Executioner",
-            Description = "Bonus damage vs low HP enemies"
+            Description = "+25% damage vs enemies below 30% HP"
         };
     }
 }
